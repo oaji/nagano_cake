d87f238ea08f04a0040a1fa452c223b91a1bf307
vagrant <vagrant@localhost.localdomain>
2020-10-06 10:21:16 +0000

    item

--- app/controllers/customers/items_controller.rb
+++ app/controllers/customers/items_controller.rb
@@ -9,14 +9,16 @@ class Customers::ItemsController < ApplicationController
   def index
 
     @genres = Genre.where(is_active: true)
-    @items = []
-      @genres.each do |genre|
-        genre.items.each do |item|
+    @array = []
+    @genres.each do |genre|
+      genre.items.each do |item|
         if item.is_active == true
-            @items.push(item)
+            @array.push(item)
         end
       end
     end
+
+  @items = Kaminari.paginate_array(@array).page(params[:page]).per(8)
 
   end
 
