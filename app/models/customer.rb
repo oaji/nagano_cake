afa22b0bbb43dfcf5aa4ae0c305d5735c12dedf2
takerushimmachi <64963177+takerushimmachi@users.noreply.github.com>
2020-10-07 15:36:59 +0900

    takechan

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -11,12 +11,12 @@ class Customer < ApplicationRecord
 
 
   validates :first_name, presence: true
-  validates :first_name_kana, presence: true
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
   validates :family_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :post_code,numericality: { only_integer: true }
+  validates :family_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+  validates :post_code, presence: true, numericality: { only_integer: true }
   validates :address, presence: true
-  validates :telephone, presence: true
+  validates :telephone, presence: true, numericality: { only_integer: true }
 
 
 
