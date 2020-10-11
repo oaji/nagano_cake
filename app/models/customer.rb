a629233bcec21f94084fe12a854b6ef0881eba78
vagrant <vagrant@localhost.localdomain>
2020-10-11 09:05:32 +0000

    test

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,9 +5,9 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-  has_many :orders, dependent: :destroy
+  has_many :orders
   has_many :addresses, dependent: :destroy
-  has_many :cart_items
+  has_many :cart_items, dependent: :destroy
 
 
   #validates :first_name, presence: true
