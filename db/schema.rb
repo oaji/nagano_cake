ded85d2984d1c877619120849a270d1ffffe1ed2
macotonacamura <65491593+macotonacamura@users.noreply.github.com>
2020-10-13 21:34:49 +0900

    fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_12_035423) do
+ActiveRecord::Schema.define(version: 2020_10_13_121449) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(version: 2020_10_12_035423) do
     t.string "post_code"
     t.string "address"
     t.string "telephone"
-    t.boolean "is_valid", default: true, null: false
     t.boolean "is_deleted", default: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
