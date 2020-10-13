d4ec3695ed64776b9d7c8886220283761fc03522
macotonacamura <65491593+macotonacamura@users.noreply.github.com>
2020-10-13 19:24:06 +0900

    finish/admin/orders_list/index/show

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,19 +1,23 @@
 class Admin::OrderItemsController < ApplicationController
 
+
 	def update
-  	@order = Order.find(params[:id])
+  	@order_item = OrderItem.find(params[:id])
+  	@order = @order_item.order
   	@order_items = @order.order_items
-    @order.update(order_item_params)
-  	  if @order.order_status == "着手不可"
-         @order.order_status.update_all(order_status:0)
-       elsif @order.order_status == "製作待ち"
-         @order.order_status.update_all(order_status:1)
-       elsif @order.order_status == "製作中"
-         @order.order_status.update_all(order_status:2)
-       elsif @order.order_status == "製作完了"
-         @order.order_status.update_all(order_status:3)
+    @order_item.order_status = params[:order_item]["order_status"]
+    if @order_item.save!
+  	  if @order_items.all?{|order_item| order_item.order_status == "着手不可"}
+         @order.update(order_status:0)
+       elsif @order_items.all?{|v| v.order_status == "製作待ち"}
+         @order.update(order_status:1)
+       elsif @order_items.all?{|v| v.order_status == "製作中"}
+       	@order.update(order_status:2)
+       elsif @order_items.all?{|v| v.order_status == "製作完了"}
+         @order.update(order_status:3)
       end
-	       redirect_to request.referer
+     end
+	     redirect_to admin_order_path(@order)
   end
 
   private
